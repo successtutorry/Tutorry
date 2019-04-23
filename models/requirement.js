ce47017252489d9dfba09b7530d8c2bb7229f5b4
surendrap720 <surendrap720@gmail.com>
2019-04-23 13:41:11 +0530

    requirement file to save in database

---
+++ models/requirement.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const requirementSchema = new Schema({
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+	location: String,
+	class: String,
+}, {
+
+	timestamps: { // this will give us the detail when the form was send.
+		createdAt: 'createdAt',
+		updatedAt: 'updatedAt'
+	}
+});
+
+
+const requirementForm = mongoose.model('requirement', requirementSchema);
+module.exports = requirementForm;
