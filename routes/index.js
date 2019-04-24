16a5ca635c55b0075760ba2b0e7716cdec51f06a
surendrap720 <surendrap720@gmail.com>
2019-04-25 02:00:50 +0530

    displaying tutor from database

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const tutor = require('../models/tutorregistration');
 //var keystone = require('keystone');
 
  /*GET home page.*/
-router.get('/', function(req, res, next) {
-  
+/*router.get('/', function(req, res, next) {
+
   res.render('index', { title:'Tutorry' });
+});*/
+
+router.get('/', (req, res) => {
+
+      tutor.find( { }, function(err, docs){
+      var tutorChunks = [];
+      var chunkSize = 3;
+      for(var i=0; i < docs.length; i+= chunkSize){
+          tutorChunks.push(docs.slice(i, i+chunkSize));
+      }
+        res.render('index', {  tutors: tutorChunks });
+      });
 });
 
 
